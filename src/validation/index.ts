fada18f0322828176eeea1c9ac517c4e29e45767
Amirhoseinmiri <amirhoseinmiri444@gmail.com>
2025-06-10 14:58:30 +0330

    fix validation number

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -4,6 +4,6 @@ export const phoneNumberSchema = z.object({
   phoneNumber: z
     .string()
     .min(10, "Phone number should be at least 10 digits")
-    .max(10, "Phone number should not exceed 10 digits")
+    .max(11, "Phone number should not exceed 10 digits")
     .regex(/^[0-9]+$/, "Phone number should only contain numbers"),
 });
